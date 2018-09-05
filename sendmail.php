9b9b1367bb8c1647b5208e1faccff9752bd4b432
YashashreeTakne <ystakne@gmail.com>
2018-09-05 12:35:20 +0530

    latest changes

---
+++ sendmail.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST["submit"])) {
+$recipient = "ystakne@gmail.com"; //my email
+echo $subject = 'Email message from Point Plumbing';
+echo $name = $_POST ["name"];
+echo $email = $_POST["email"];
+echo $phone = $_POST["number"];
+echo  $message = $_POST["message"];
+
+ $mailBody="Name: $name\nEmail: $email\n\n$message"; 
+
+ mail($recipient, $subject, $mailBody, "From: $name <$email>");
+
+echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
+
+}
+?>
